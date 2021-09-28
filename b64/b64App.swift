ed1394fc3a57eb4ad9256a006257f83f29ec5ea2
Siddharth <ahoy@barelyhuman.dev>
2021-09-28 06:46:02 +0530

    Initial Commit

---
+++ b64/b64App.swift
@@ -0,0 +1,17 @@
+//
+//  b64App.swift
+//  b64
+//
+//  Created by Reaper on 28/09/21.
+//
+
+import SwiftUI
+
+@main
+struct b64App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
